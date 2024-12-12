5766bcb7c12c247cd851e420485ee475ff19a507
Tiwi Mustika Dewi <tiwi.md21@gmail.com>
2024-12-12 09:33:55 +0700

    Menambahkan void getConsoleSize
    Untuk mengatur pergerakan dalam permainan

--- FeedingFrenzy.cpp
+++ FeedingFrenzy.cpp
@@ -193,6 +193,12 @@ bool SignIn() {
         return false;
     }
 }
+void getConsoleSize(int &width, int &height) {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+}
 
 void Logic()                           
 {
